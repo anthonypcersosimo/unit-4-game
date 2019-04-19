7d1ec612494c915da0f84e708b1155a2dc9927d9
Anthony <anthony.cersosimo@gmail.com>
2019-04-19 11:56:30 -0400

    added background images to header and instructions body, styled the target number / W/L / total score boxes, added jQuery to give the target number a random number and print it to the screen, created variables inside a document.read function for each crystal which will only recalculate upon opening the page, created on click event listeners for each of the four crystals on the page with their own random number values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,33 @@
 //Declare variables
-var targetNumber = Math.random();
+var targetNumber = Math.floor((Math.random() * 120) + 19);
+var wins = 0;
+var losses = 0;
+var yourScore = 0;
 
-$(window).mousemove(function(event) {
-  
-    $("#miner").css({top: event.pageY, left: event.pageX});
-});
+
+$(document).ready(function() {
+    var yellowCrystal =  Math.floor((Math.random() * 12) + 1);
+    var redCrystal =  Math.floor((Math.random() * 12) + 1);
+    var greenCrystal =  Math.floor((Math.random() * 12) + 1);
+    var blueCrystal =  Math.floor((Math.random() * 12) + 1);
+    console.log(yellowCrystal);
+    console.log(redCrystal);
+    console.log(greenCrystal);
+    console.log(blueCrystal);
+    
+    $("#target-number").html("<p>Target Number: " + targetNumber + "</p>");
+    $("#win-loss").html("<p>Wins: " + wins + " " + "Losses: " + losses + "</p>");
+
+    $(".top-right").on("click", function(){
+        console.log(yellowCrystal);
+    }) 
+    $(".top-mid").on("click", function(){
+        console.log(redCrystal);
+    }) 
+    $(".bottom-mid").on("click", function(){
+        console.log(greenCrystal);
+    }) 
+    $(".bottom-right").on("click", function(){
+        console.log(blueCrystal);
+    }) 
+})
